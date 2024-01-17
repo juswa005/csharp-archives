42667b2aab4ab3c775e074705515cbde469d55d0
juswa005 <amieljosh05@gmail.com>
2024-01-17 10:30:00 +0800

    OOP: Variables and Data Types

---
+++ OOP_Object-Oriented-Programming/lessons/variables-and-datatypes.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age = 20;
+        double grade = 89.75;
+        char letter = 'A';
+        string name = "Amiel";
+        bool isStudent = true;
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Grade: " + grade);
+        Console.WriteLine("Letter Grade: " + letter);
+        Console.WriteLine("Is Student: " + isStudent);
+    }
+}
+
