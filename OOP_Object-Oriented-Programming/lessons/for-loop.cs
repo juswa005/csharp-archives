6533c43b3b459139391495c6b2f33098737715f0
juswa005 <amieljosh05@gmail.com>
2024-02-01 14:45:00 +0800

    OOP: For Loop

---
+++ OOP_Object-Oriented-Programming/lessons/for-loop.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int i;
+
+        for (i = 1; i <= 5; i++)
+        {
+            Console.WriteLine("Count: " + i);
+        }
+    }
+}
+
